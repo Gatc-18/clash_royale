be8578d7137a904c4b522b5d903f050b54ead6e1
gatc-18 <tovargusta3@gmail.com>
2022-03-08 13:51:15 -0500

    initial commit

---
+++ data/data.js
@@ -0,0 +1,67 @@
+let cards = [
+    {
+        id: 1,
+        img: "../images/archer-queen.webp",
+    },
+    {
+        id: 2,
+        img: "../images/arrows.webp",
+    }
+    ,
+    {
+        id: 3,
+        img: "../images/baby-dragon.webp",
+    },
+    {
+        id: 4,
+        img: "../images/balloon.webp",
+    },
+    {
+        id: 5,
+        img: "../images/barbarian-barrel.webp",
+    },
+    {
+        id: 6,
+        img: "../images/barbarians.webp",
+    },
+    {
+        id: 7,
+        img: "../images/clone.webp",
+    },
+    {
+        id: 8,
+        img: "../images/electro-spirit.webp",
+    },
+    {
+        id: 9,
+        img: "../images/electro-spirit.webp",
+    },
+    {
+        id: 10,
+        img: "../images/fireball.webp",
+    },
+    {
+        id: 11,
+        img: "../images/graveyard.webp",
+    },
+    {
+        id: 12,
+        img: "../images/heal-spirit.webp",
+    },
+    {
+        id: 13,
+        img: "../images/lava-hound.webp",
+    },
+    {
+        id: 14,
+        img: "../images/lumberjack.webp",
+    },
+    {
+        id: 15,
+        img: "../images/magic-archer.webp",
+    },
+    {
+        id: 16,
+        img: "../images/miner.webp",
+    }
+]
